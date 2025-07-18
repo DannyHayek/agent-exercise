cc2a6bd14385953369619b7391d77b7db7231900
Danny <dhayek92@gmail.com>
2025-07-18 12:54:54 +0300

    Created website model with relevant derivates

--- database/migrations/2025_07_18_092305_create_websites_table.php
+++ database/migrations/2025_07_18_092305_create_websites_table.php
@@ -11,8 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('websites', function (Blueprint $table) {
             $table->id();
+            $table->integer("agent_id");
+            $table->string("url");
+            $table->string("ip_address");
             $table->timestamps();
         });
     }
@@ -22,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('websites');
     }
 };
